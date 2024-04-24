5e79cafb4295d0966e29d88c10f065cc093916d3
AdriannaLen <150341645+AdriannaLen@users.noreply.github.com>
2024-04-24 17:00:00 +0200

    added selectedFriend to FormSplitBill to change name in the form after clicking select

--- eat-and-split/src/App.js
+++ eat-and-split/src/App.js
@@ -62,7 +62,7 @@ export default function App() {
 
     </div>
 
-    { selectedFriend && <FormSplitBill />}
+    { selectedFriend && <FormSplitBill selectedFriend={selectedFriend}/>}
 
     </div>
 
@@ -164,11 +164,11 @@ function FormAddFriend({onAddFriend}) {
   );
 }
 
-function FormSplitBill() {
+function FormSplitBill({selectedFriend}) {
 
   return (  <form className="form-split-bill">
 
-<h2>Split a bill with X</h2>
+<h2>Split a bill with {selectedFriend.name} </h2>
 
       <label>Bill value</label>
       <input type="text" />
